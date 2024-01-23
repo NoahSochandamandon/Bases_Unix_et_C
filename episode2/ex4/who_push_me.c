f4e20b2fb622693deb273720b401bf2d2a45c57f
wsl ubuntu <noah.sochandamandon@gmail.com>
2024-01-23 11:41:20 +0100

    amandine who_push_me

--- episode2/ex4/who_push_me.c
+++ episode2/ex4/who_push_me.c
@@ -1,22 +1,87 @@
+// #include <unistd.h>
+// #include <stdio.h>
+// #include <signal.h>
+// #include <string.h>
+
+// void fin(int sig);
+
+// int main(int argc, char const *argv[])
+// {
+//     printf("PID: %d\n", getpid());
+
+//     const int SIG[] = {SIGUSR1, SIGUSR2, SIGINT, SIGQUIT};
+//     int i = 0;
+
+//     while (i < 4)
+//     {
+//         signal(SIG[i], fin);
+//         i++;
+//     }
+
+//     while (1)
+//     {
+//         pause();
+//     }
+
+//     return 0;
+// }
+
+// void fin(int sig)
+// {
+//     printf("Signal reçu: %d\n", sig);
+
+//     // char pid;
+//     // char *desc = strsignal(sig);
+
+//     char *type = "inconnu";
+
+//     if (sig == SIGUSR1)
+//     {
+//         type = "received SIGUSR1";
+//     }
+//     else if (sig == SIGUSR2)
+//     {
+//         type = "received SIGUSR2";
+//     }
+//     else if (sig == SIGINT)
+//     {
+//         type = "received SIGINT";
+//     }
+//     else if (sig == SIGQUIT)
+//     {
+//         type = "received SIGQUIT";
+//     }
+
+//     printf("[FROM] %s (Signal) %d", type, sig);
+// }
+
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <asm-generic/siginfo.h>
 
-void fin(int sig);
+void function(int sig, siginfo_t *info, void *context)
+{
+    int signal_pid = info->si_pid;
+    printf("FROM[%i] receveid %i (%s)\n", signal_pid, sig, strsignal(sig))
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("PID: %d\n", getpid());
+    struct sigaction sa;
 
-    const int SIG[] = {SIGUSR1, SIGUSR2, SIGINT, SIGQUIT};
-    int i = 0;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = function;
 
-    while (i < 4)
-    {
-        signal(SIG[i], fin);
-        i++;
-    }
+    printf("PID : %i\n", getpid());
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
 
     while (1)
     {
@@ -24,33 +89,4 @@ int main(int argc, char const *argv[])
     }
 
     return 0;
-}
-
-void fin(int sig)
-{
-    printf("Signal reçu: %d\n", sig);
-
-    // char pid;
-    // char *desc = strsignal(sig);
-
-    char *type = "inconnu";
-
-    if (sig == SIGUSR1)
-    {
-        type = "received SIGUSR1";
-    }
-    else if (sig == SIGUSR2)
-    {
-        type = "received SIGUSR2";
-    }
-    else if (sig == SIGINT)
-    {
-        type = "received SIGINT";
-    }
-    else if (sig == SIGQUIT)
-    {
-        type = "received SIGQUIT";
-    }
-
-    printf("[FROM] %s (Signal) %d", type, sig);
 }
